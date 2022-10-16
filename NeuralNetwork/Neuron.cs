30b04a5e6010283725f643416f5fcf691fd49de9
Igor <greyjedi2@gmail.com>
2022-10-16 18:17:43 +0600

    init

---
+++ NeuralNetwork/Neuron.cs
@@ -0,0 +1,27 @@
+﻿namespace NeuralNetwork
+{
+    public class Neuron
+    {
+        public List<double> Weigths { get; }
+        public NeuronType NeuronType { get; }
+        public double Output { get; private set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="inputCount">Количество входящих нейронов</param>
+        /// <param name="neuronType">Тип нейрона</param>
+        public Neuron(int inputCount, NeuronType neuronType = NeuronType.Normal)
+        {
+            NeuronType = neuronType;
+            Weigths = new List<double>();
+
+            for (int i = 0; i < inputCount; i++)
+            {
+
+            }
+        }
+
+
+    }
+}
